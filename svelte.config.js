f466a302599f7ed20b455b34c9470b64d959d7a1
Varun Chowdary <vrcreddy02@gmail.com>
2025-09-21 16:22:11 -0400

    Configure for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = process.env.NODE_ENV === 'production' ? '/MyWebsitePortfolio' : '';
+const base = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -24,7 +24,7 @@ const config = {
 			'@utils': './src/lib/utils'
 		},
 		paths: {
-			base
+			base: base
 		}
 	}
 };
